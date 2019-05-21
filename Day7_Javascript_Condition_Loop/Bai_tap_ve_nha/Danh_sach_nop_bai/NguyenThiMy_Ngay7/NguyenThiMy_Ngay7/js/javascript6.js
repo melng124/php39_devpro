bb25a5ce0ba4705affa45ee26519befc92c03a54
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2019-05-21 17:33:58 +0700

    nhan xet

--- Day7_Javascript_Condition_Loop/Bai_tap_ve_nha/Danh_sach_nop_bai/NguyenThiMy_Ngay7/NguyenThiMy_Ngay7/js/javascript6.js
+++ Day7_Javascript_Condition_Loop/Bai_tap_ve_nha/Danh_sach_nop_bai/NguyenThiMy_Ngay7/NguyenThiMy_Ngay7/js/javascript6.js
@@ -1,13 +1,13 @@
 function kiemTraChuoi() {
-    var chuoi1=document.getElementById('chuoithunhat').value;
-    var chuoi2=document.getElementById('chuoithuhai').value;
-    var chuoi3=document.getElementById('ketqua');
-    if(chuoi1.includes(chuoi2)==-1){
+    var chuoi1 = document.getElementById('chuoithunhat').value;
+    var chuoi2 = document.getElementById('chuoithuhai').value;
+    var chuoi3 = document.getElementById('ketqua');
+    if (chuoi1.includes(chuoi2) === -1) {
 
-        document.getElementById('ketqua').innerHTML="Không tồn tại";
+        document.getElementById('ketqua').innerHTML = "Không tồn tại";
     }
     else {
-        document.getElementById('ketqua').innerHTML="chuỗi " + chuoi2 + " tồn tại trong chuỗi ban đầu " + chuoi1;
+        document.getElementById('ketqua').innerHTML = "chuỗi " + chuoi2 + " tồn tại trong chuỗi ban đầu " + chuoi1;
     }
 
 
